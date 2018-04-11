d7939dc87d47c06d94af7e8806d1b6346e8faceb
Simon Bear <simon.bear@csiro.au>
2018-04-11 18:53:13 +1000

    Add DataSourceHealthIndicator

--- grails-app/init/apikey/Application.groovy
+++ grails-app/init/apikey/Application.groovy
@@ -2,9 +2,12 @@ package apikey
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
+import org.springframework.boot.actuate.health.DataSourceHealthIndicator
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.session.data.redis.config.ConfigureRedisAction
+
+import javax.sql.DataSource
 
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
@@ -16,4 +19,10 @@ class Application extends GrailsAutoConfiguration {
     ConfigureRedisAction configureRedisAction() {
         ConfigureRedisAction.NO_OP
     }
+
+    @Bean
+    DataSourceHealthIndicator dataSourceHealthIndicator(DataSource dataSource) {
+        new DataSourceHealthIndicator(dataSource)
+    }
+
 }
